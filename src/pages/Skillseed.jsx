6ffe0e214254b56663795b5c6cf313f57825507b
Juliet <ohankwerejulietkelechi@gmail.com>
2026-05-22 05:47:34 +0200

    updates for faq and contact form

--- src/pages/Skillseed.jsx
+++ src/pages/Skillseed.jsx
@@ -131,20 +131,7 @@ const Skillseed = () => {
               target="_blank"
               className="flex items-center justify-center md:justify-start"
             >
-              {/* <motion.button
-            onClick={scrollToMore}
-            className="w-[171px] h-[50px] text-white font-primarySemibold md:font-primaryMedium bg-[#FAB548] rounded-full my-4 uppercase flex items-center justify-center text-sm md:text-base font-cocon cursor-pointer"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
-          >
-            Take Quiz
-            <span>
-              <IoArrowForwardCircle className="mx-2" />
-            </span>
-          </motion.button> */}
               <motion.button
-                onClick={scrollToMore}
                 className="w-[171px] h-[50px] text-white font-primarySemibold md:font-primaryMedium bg-[#FAB548] rounded-full my-4 uppercase flex items-center justify-center text-sm md:text-base font-cocon cursor-pointer"
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
@@ -161,20 +148,7 @@ const Skillseed = () => {
               target="_blank"
               className="flex items-center justify-center md:justify-start"
             >
-              {/* <motion.button
-            onClick={scrollToMore}
-            className="w-[171px] h-[50px] text-white font-primarySemibold md:font-primaryMedium bg-[#FAB548] rounded-full my-4 uppercase flex items-center justify-center text-sm md:text-base font-cocon cursor-pointer"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
-          >
-            See More
-            <span>
-              <IoArrowForwardCircle className="mx-2" />
-            </span>
-          </motion.button> */}
               <motion.button
-                onClick={scrollToMore}
                 className="w-[171px] h-[50px] text-white font-primarySemibold md:font-primaryMedium bg-[#FAB548] rounded-full my-4 uppercase flex items-center justify-center text-sm md:text-base font-cocon cursor-pointer"
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
